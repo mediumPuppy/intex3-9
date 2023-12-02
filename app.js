a898da040ddbc28ede3acd5cb6df4df2fe466c52
JL <lambertjefferson66@gmail.com>
2023-12-02 10:29:31 -0700

    db setup correctly

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 let express = require('express');
 let knexInit = require('knex');
 let path = require('path');
+require('dotenv').config();
 
 // declare app
 let app = express();
@@ -15,25 +16,29 @@ let PORT = process.env.PORT || 3000
 let DB_PORT = process.env.RDS_PORT || process.env.DB_PORT || 5432
 
 // set up db connection
-// let knex = knexInit({
-//   client: 'pg',
-//   connection: {
-//     host: 'testhost',
-//     user: 'testuser'r,
-//     password: 'testpass',
-//     database: 'testdb',
-//     port: DB_PORT
-//   }
-// });
+let knex = knexInit({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: process.env.DB_SSL ? {rejectUnauthorized: false} : false
+  }
+});
 
 // test db connection with generic query
-// knex.raw('SELECT NOW()')
-//   .then(result => {
-//     console.log('Connection successful, current time is:', result.rows[0].now);
-//   })
-//   .catch(error => {
-//     console.error('Connection failed:', error);
-//   });
+knex.raw('SELECT NOW()')
+  .then(result => {
+    console.log('Connection successful.' + result.rows[0].now);
+  })
+  .catch(error => {
+    console.error('Connection failed:', error);
+  })  .finally(() => {
+    knex.destroy();
+    // Close the connection
+  });
 
 app.get('/', (req,res) => {
   res.render("landing")
