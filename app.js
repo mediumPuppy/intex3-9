64e35abfaf161178d07650e5da19155444ddcc6b
JL <lambertjefferson66@gmail.com>
2023-12-04 11:01:36 -0700

    post req for adding users, sanitization and validation of inputs

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ let express = require('express');
 let knexInit = require('knex');
 let path = require('path');
 require('dotenv').config();
+const { body, validationResult } = require('express-validator');
+
 
 // declare app
 let app = express();
 
 // 'set' and 'use' here
 app.set('view engine', 'ejs');
+app.use(express.static('static'));
 app.use(express.urlencoded({ extended: true}));
 
 // set env variables
-let PORT = process.env.PORT || 3000
-let DB_PORT = process.env.RDS_PORT || process.env.DB_PORT || 5432
+let PORT = process.env.PORT || 3000;
+let DB_PORT = process.env.RDS_PORT || process.env.DB_PORT || 5432;
 
 // set up db connection
 let knex = knexInit({
@@ -52,9 +55,45 @@ app.get('/login', (req,res) => {
 
 // });
 
-// app.post('/adduser', (req,res) => {
 
-// });
+
+
+app.post('/create', 
+  [
+    body('username').trim().isLength({ min: 3 }).escape(),
+    body('password').isLength({ min: 5 }),
+    body('firstname').trim().isAlpha().escape(),
+    body('lastname').trim().isAlpha().escape(),
+  ], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  try {
+    const foundUsers = await knex('users').where({ username: username });
+    if (foundUsers.length > 0) {
+      return res.redirect(`/admin?status=username`);
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await knex('users').insert({
+      username,
+      passwordHash: hashedPassword,
+      firstname,
+      lastname
+    });
+
+    // Redirect or send a success response
+    return res.redirect(`/admin?status=created`);
+  } catch (error) {
+    // Log the error and send an error response
+    console.error(error);
+    return res.status(500).send('An error occurred');
+  }
+});
+
 
 // app.get('/deleteuser', (req,res) => {
 
