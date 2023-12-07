b6f83b86e99787e5a454b62be817007f17ecd327
JL <lambertjefferson66@gmail.com>
2023-12-06 22:28:41 -0700

    hope this works

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d intex39.is404.net --nginx --agree-tos --email lambertjefferson66@gmail.com
--- app.js
+++ app.js
@@ -28,17 +28,6 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-//make session user var available to all views
-app.use((req, res, next) => {
-  if (req.session.user_id) {
-    res.locals.user_id = req.session.user_id;
-  } else {
-    res.locals.user_id = null;
-  }
-  next();
-});
-
-
 // set env variables
 let PORT = process.env.PORT || 3000;
 let DB_PORT = process.env.DB_PORT || 5432;
@@ -71,7 +60,7 @@ app.get('/', (req,res) => {
 
 app.get('/login', (req, res) => {
   // If user is already logged in, redirect to account page
-  if (req.session.user_id == 'admin') {
+  if (req.session.user_id) {
     return res.redirect('/account');
   }
 
@@ -83,109 +72,35 @@ app.get('/login', (req, res) => {
   res.render('login', { statusMessage: statusMessage });
 });
 
-// edit user from admin page
-app.get('/edituser/:id', async (req, res) => {
-  const userId = req.params.id;
-
-  try {
-    const user = await knex('users').where('id', userId).first();
-    if (user) {
-      res.render('edituser', { user }); // render edit user page with user data
-    } else {
-      res.status(404).send('User not found');
-    }
-  } catch (error) {
-    res.status(500).send('Server error');
-  }
-});
-
-// actually post the changed data from the user id
-app.post('/edituser/:id', async (req, res) => {
-  const userId = req.params.id;
-  const updatedData = req.body; // Assuming the body contains the updated user data
-  try {
-    await knex('users').where('id', userId).update(updatedData);
-    res.redirect('/admin'); // Redirect to a confirmation page or user list
-  } catch (error) {
-    res.status(500).send('Server error');
-  }
-});
-
-// admin ability to delete user
-app.get('/deleteuser/:id', async (req, res) => {
-  const userId = req.params.id;
-  try {
-    await knex('users').where('id', userId).del();
-    res.redirect('/admin'); // Redirect to the list of users, or another appropriate page
-  } catch (error) {
-    res.status(500).send('Server error');
-  }
-});
-
-
-
-
 //admin route for seeing all the data
-app.get('/data', async (req, res) => {
-
+app.get('/admin/data', async (req, res) => {
   let respondents = await knex('respondent');
   
-  res.render('data', {data:respondents})
-} 
+  res.render('data', {data:respondents})}
 )
 
 //a view to see one of the user's data while an admin
-app.get('/data/:userid', async (req, res) => {
-  // if (req.session.user_id == 'admin') {
+app.get('/admin/data/:userid', async (req, res) => {
   try {
     let individual = await knex('respondent').where('user_id', '=', req.params.userid);
-    res.render('data', {data:individual})
+    res.render('individuals', {data:individual})
   }
 
   catch (error) {
     console.error(error);
     res.status(500).send('Server error');
   }
-  // } else {
-  //   res.redirect('access')
-  // }
-});
+}
+)
 // admin route for creating users
-app.get('/admin', async (req, res) => {
-  const users = await knex('users')
-  res.render('admin', { 
-                        query: req.query,
-                        data: users       
-                      })
-});
-
-// this route will redirect to the resources view
-app.get('/resources', (req, res) => {
-  res.render('resources')
-});
-
-//this route will redirect to the about view
-app.get('/about', (req, res) => {
-  res.render('about')
-});
-
-//this route will redirect to the learn view with the dashboard
-app.get('/learn', (req, res) => {
-  res.render('learn')
+app.get('/admin', (req, res) => {
+  // const data = await knex('all data') ...  make async 
+  res.render('admin', { query: req.query })
 });
 
 app.get('/survey', (req, res) => {
-  // Check if the 'submitted' query parameter exists
-  const submitted = req.query.submitted === 'true';
-
-  // Render the 'survey' view and pass the 'submitted' flag
-  res.render('survey', { submitted: submitted });
+  res.render('survey')
 });
-
-
-app.get('/access', (req, res) => {
-  res.status(403).render('access')
-})
 
 app.get('/account', async (req, res) => {
   if (!req.session.user_id) {
@@ -252,7 +167,7 @@ app.post('/create',
     body('password').isLength({ min: 5 }),
     body('firstname').trim().isAlpha().escape(),
     body('lastname').trim().isAlpha().escape(),
-    body('email').trim().isEmail().normalizeEmail()
+    //! insert email here 
   ], 
   async (req, res) => {
 
@@ -286,7 +201,8 @@ app.post('/create',
 });
 
 app.post('/update-account', async (req, res) => {
-  const { email, firstname, lastname, password } = req.body;
+  // Extract data from the request body
+  const { email, firstname, lastname } = req.body;
   const userId = req.session.user_id; // Assuming you store user id in session after login
 
   if (!userId) {
@@ -294,36 +210,21 @@ app.post('/update-account', async (req, res) => {
   }
 
   try {
-
-    const updateFields = {
-      email: email,
-      firstname: firstname,
-      lastname: lastname, // Save the hashed password
-    }
-    // Hash the password before saving it to the database
-    if (password) {
-      // Hash the new password
-      const saltRounds = 10;
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
-      // Update the password field
-      updateFields.passwordhash = hashedPassword;
-    }
-
     // Update the user in the database
     await knex('users')
       .where({ id: userId })
-      .update(updateFields);
+      .update({
+        email: email,
+        firstname: firstname,
+        lastname: lastname
+      });
 
-    // Redirect or send a success response
-    res.redirect('/account');
+    res.redirect('/account'); // Redirect back to account page after update
   } catch (error) {
-    // Handle errors (e.g., database errors)
     console.error(error);
     res.status(500).send('Unable to update account information');
   }
 });
-
-
 
 
 
@@ -334,16 +235,16 @@ app.post('/update-account', async (req, res) => {
 // app.get('edituser', (req,res) => {
 
 // });
-app.post('/submitsurvey', async (req, res) => {
+app.post('/', async (req, res) => {
   try {
     const {
       age,
       gender,
       relationship_status,
       occupation_status,
-      organization_affiliation, // This will be an array
+      organization_affiliation,
       social_media_user,
-      social_media_platforms, // This will also be an array
+      social_media_platforms,
       daily_social_media_time,
       use_without_purpose,
       use_while_busy,
@@ -352,78 +253,45 @@ app.post('/submitsurvey', async (req, res) => {
       bothered_by_worries,
       difficult_to_concentrate,
       compare_with_successful_people,
-      feelings_toward_comparisons,
+      feel_about_comparisons,
       seek_validation,
       feelings_of_depression,
       fluctuation_of_interest_in_activities,
       sleep_issue_frequency,
     } = req.body;
 
-    // get a timestamp to insert the data. 
-    let time = formatCurrentDateTime()
-    // Insert data into the "respondents" table and get the inserted User_ID
-    const insertedIds = await knex('respondent').insert({
-    age: age,
-    gender: gender,
-    timestamp: time,
-    relationship_status: relationship_status,
-    occupation_status: occupation_status,
-    social_media_user: social_media_user,
-    region: 'Provo',
-    daily_social_media_time: daily_social_media_time,
-    use_without_purpose: use_without_purpose,
-    use_while_busy: use_while_busy,
-    restlessness_from_withdrawal: restlessness_from_withdrawal,
-    easily_distracted: easily_distracted,
-    bothered_by_worries: bothered_by_worries,
-    difficult_to_concentrate: difficult_to_concentrate,
-    compare_with_successful_people: compare_with_successful_people,
-    feelings_toward_comparisons: feelings_toward_comparisons,
-    seek_validation: seek_validation,
-    feelings_of_depression: feelings_of_depression,
-    fluctuation_of_interest_in_activities: fluctuation_of_interest_in_activities,
-    sleep_issue_frequency: sleep_issue_frequency
-
-    }).returning('user_id');
-
-
-    //! SET THIS TO DO THE ROLLBACK JOSH MENTIONED. 
-    const userId = insertedIds[0].user_id;
-
-
-
-    // Assuming organization_affiliation and social_media_platforms are arrays
-    for (const orgId of organization_affiliation) {
-      for (const socialMediaId of social_media_platforms) {
-        await knex('linking').insert({
-          user_id: userId,
-          organization_id: orgId,
-          social_platform_id: socialMediaId
-        });
-      }
-    }
+    // Insert data into the "respondents" table
+    await knex('respondents').insert({
+      age,
+      gender,
+      relationship_status,
+      occupation_status,
+      organization_affiliation,
+      social_media_user,
+      social_media_platforms,
+      daily_social_media_time,
+      use_without_purpose,
+      use_while_busy,
+      restlessness_from_withdrawal,
+      easily_distracted,
+      bothered_by_worries,
+      difficult_to_concentrate,
+      compare_with_successful_people,
+      feel_about_comparisons,
+      seek_validation,
+      feelings_of_depression,
+      fluctuation_of_interest_in_activities,
+      sleep_issue_frequency,
+      region: 'provo', // Set "region" to "provo"
+    });
 
     console.log('Data inserted successfully');
-    res.redirect('/survey?submitted=true');
+    res.send('Form submitted successfully');
   } catch (error) {
     console.error('Error inserting data: ' + error.message);
     res.status(500).send('Error submitting the form');
   }
 });
-
-function formatCurrentDateTime() {
-  var now = new Date();
-
-  var year = now.getFullYear();
-  var month = ('0' + (now.getMonth() + 1)).slice(-2); // months are zero-indexed
-  var day = ('0' + now.getDate()).slice(-2);
-
-  var hours = ('0' + now.getHours()).slice(-2);
-  var minutes = ('0' + now.getMinutes()).slice(-2);
-  var seconds = ('0' + now.getSeconds()).slice(-2);
-
-  return month + '/' + day + '/' + year + ' ' + hours + ':' + minutes + ':' + seconds;
-}
 
 // end
 app.listen(PORT, (req, res) => {
